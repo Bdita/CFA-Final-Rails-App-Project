c7e0075658a71c67ac9ebd6613343a4553931ffd
Bdita <fpbandita@gmail.com>
2017-04-19 13:10:08 +1000

    image resize 600X600

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  # process resize_to_limit: [600, 600]
+
+  process resize_to_limit: [600, 600]
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick

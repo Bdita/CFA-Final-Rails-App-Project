a7ad01fa3696da43ebb3f1fac728a5212d7c1657
Bdita <fpbandita@gmail.com>
2017-04-20 23:51:20 +1000

    admin and ngo dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   devise_for :users
   root 'pages#home'
-
+  get 'admindashboard', to: 'pages#admindashboard'
+  get 'ngodashboard', to: 'pages#ngodashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
